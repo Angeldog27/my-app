e12f1efe7ac15a323a7ca8f498d82f00ff26d9dc
Angeldog27 <126614235+Angeldog27@users.noreply.github.com>
2023-05-15 18:11:11 -0400

    Updated for Github

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import "./App.css";
 import Search from "./Search";
+import "./Search.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Weather App</h1>
+        <h2>Weather App</h2>
         <Search />
       </header>
     </div>
--- src/Search.js
+++ src/Search.js
@@ -1,10 +1,13 @@
 import React, { useState } from "react";
 import axios from "axios";
+import ReactAnimatedWeather from "react-animated-weather";
+import "./Search.css";
 
 export default function Search() {
   let [city, setCity] = useState("");
   let [loaded, setLoaded] = useState(false);
   let [weather, setWeather] = useState({});
+  let [message, setMessage] = useState("");
 
   function showWeather(response) {
     setLoaded(true);
@@ -18,6 +21,7 @@ export default function Search() {
 
   function handleSubmit(event) {
     event.preventDefault();
+    setMessage(`${city}`);
     let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=082d3d02ffdb12f2fd9b259e2ced1d0d&units=metric`;
     axios.get(url).then(showWeather);
   }
@@ -40,10 +44,66 @@ export default function Search() {
       <div>
         {form}
         <ul>
+          <li>
+            <h3>{message}</h3>
+            <ReactAnimatedWeather
+              icon="CLEAR_DAY"
+              color="goldenrod"
+              size={55}
+              animate="true"
+            />
+          </li>
           <li> Temperature: {weather.temperature}°C</li>
           <li> Humidity: {weather.humidity}%</li>
           <li> Wind: {weather.wind} m/h</li>
         </ul>
+        <ul>
+          <li className="days">Monday </li>
+          <li className="days">Tuesday </li>
+          <li className="days">Wednesday </li>
+          <li className="days">Thursday </li>
+          <li className="days">Friday </li>
+        </ul>
+        <div>
+          <ReactAnimatedWeather
+            icon="CLOUDY"
+            color="goldenrod"
+            size={50}
+            animate="true"
+          />
+          <ReactAnimatedWeather
+            icon="CLEAR_DAY"
+            color="goldenrod"
+            size={50}
+            animate="true"
+          />
+          <ReactAnimatedWeather
+            icon="RAIN"
+            color="goldenrod"
+            size={50}
+            animate="true"
+          />
+          <ReactAnimatedWeather
+            icon="CLEAR_DAY"
+            color="goldenrod"
+            size={50}
+            animate="true"
+          />
+          <ReactAnimatedWeather
+            icon="FOG"
+            color="goldenrod"
+            size={50}
+            animate="true"
+          />
+        </div>
+        <ul>
+          <li className="Temp">22</li>
+          <li className="Temp">24</li>
+          <li className="Temp">21</li>
+          <li className="Temp">27</li>
+          <li className="Temp">30</li>
+        </ul>
+        <p> Open Source Code by Nicole Torres from SheCodes</p>
       </div>
     );
   } else {

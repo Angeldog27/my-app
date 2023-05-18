6cc7cceb5aa995137d2730404dacf00cb0c18864
Angeldog27 <126614235+Angeldog27@users.noreply.github.com>
2023-05-17 22:24:18 -0400

    Added github repo

--- src/App.js
+++ src/App.js
@@ -7,6 +7,15 @@ function App() {
     <div className="App">
       <header className="App-header">
         <h2>Weather App</h2>
+        <footer>
+          <a
+            href="https://github.com/Angeldog27/Weather-React "
+            target="_blank"
+            rel="noreferrer"
+          >
+            Visit my GitHub Repository
+          </a>
+        </footer>
         <Search />
       </header>
     </div>
